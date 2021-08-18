58bef807b1b5673d94fc9662510573bd30519764
CreativeBrain01 <brankin@student.neumont.edu>
2021-08-18 08:18:51 -0600

    Encryption File

--- TheActualProject/wordify.js
+++ TheActualProject/wordify.js
@@ -1,33 +1,9 @@
 const bcrypt = require('bcryptjs');
 
-//Sync
-let salt = bcrypt.genSaltSync(10);
-let hash = bcrypt.hashSync('bacon', salt);
-
-console.log(salt);
-console.log(hash);
-
-console.log(bcrypt.compareSync('bacon', hash));
-console.log(bcrypt.compareSync('veggies', hash));
-
-//Async (for REALLY complex salts)
 const makeHash = the_str => {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(the_str, salt, (err, my_hash) => {
-            console.log(salt);
-            console.log(my_hash);
-            hashComplete(my_hash);
+            return my_hash;
         });
     });
 };
-
-const hashComplete = the_hash => {
-    bcrypt.compare('bacon', the_hash, (err, res) => {
-        console.log(res);
-    });
-    bcrypt.compare('cucumber', the_hash, (err, res) => {
-        console.log(res);
-    });
-};
-
-makeHash("cucumber");
