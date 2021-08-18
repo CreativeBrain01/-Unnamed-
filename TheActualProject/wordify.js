1cec942b7fa32292c7b31ceb1bf704d864bb86a5
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2021-08-18 08:45:52 -0600

    Quickfixes to create

--- TheActualProject/wordify.js
+++ TheActualProject/wordify.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const encrpyt = the_str => {
+const encrpyt = (the_str) => {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(the_str, salt, (err, my_hash) => {
             return my_hash;
